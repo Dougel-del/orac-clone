b000bec5e866b6b913a9ea3969316223a74aab18
dougelam <dougelam0@gmail.com>
2024-06-21 18:59:14 +0700

    [UPDATE] Remove metadata of page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,4 @@
-import { Metadata } from "next";
-import { defaultSEO, generateMetadata } from "@/src/utils/seo";
 import HomeViewPage from "./views/HomeView/HomeViewPage";
-
-export const metadata: Metadata = generateMetadata({
-  title: defaultSEO.title,
-  description: defaultSEO.description,
-  keywords: defaultSEO.keywords,
-});
 
 export default function Home() {
   return <HomeViewPage />;
